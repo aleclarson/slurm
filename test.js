6cb83f95dfd6708d413371eb3a727abfbbd84962
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-11-27 16:38:20 -0500

    test: add more tests

--- test.js
+++ test.js
@@ -1,28 +1,106 @@
-const { test, beforeAll, afterEach } = require('testpass')
+const { test, ftest, beforeAll, beforeEach } = require('testpass')
 const slurm = require('slurm').default
 
-beforeAll(setErrorHandler)
-afterEach(clearArgv)
-
-test('flag after rest argument', t => {
-  setArgv(['xx', '-b', 'qq'])
-
-  const args = slurm({ b: true })
-  t.eq(args[0], 'xx')
-  t.eq(args._, 'xx -b qq')
-  t.eq(args.b, 'qq')
+beforeAll(() => {
+  slurm.error = err => {
+    throw err
+  }
 })
+
+let res, config
+beforeEach(() => {
+  res = config = undefined
+  clearArgv()
+})
+
+test('untyped flag', t => {
+  setConfig({
+    b: true,
+  })
+
+  setArgv(['-b'])
+  t.eq(res.b, true)
+
+  setArgv(['-b', 'bq'])
+  t.eq(res.b, 'bq')
+})
+
+test('string flag', t => {
+  setConfig({
+    b: { type: 'string' },
+  })
+
+  setArgv(['-b', 'bq'])
+  t.eq(res.b, 'bq')
+})
+
+test('string alias', t => {
+  setConfig({
+    branch: { type: 'string' },
+    b: 'branch',
+  })
+
+  setArgv(['-b', 'bq'])
+  t.eq(res.branch, 'bq')
+})
+
+test('boolean flag with no value', t => {
+  setConfig({
+    f: { type: 'boolean' },
+    x: { type: 'boolean' },
+  })
+
+  setArgv(['-f', '-x'])
+  t.eq(res.f, true)
+  t.eq(res.x, true)
+})
+
+test('boolean alias with no value', t => {
+  setConfig({
+    force: { type: 'boolean' },
+    f: 'force',
+    x: { type: 'boolean' },
+  })
+
+  setArgv(['-f', '-x'])
+  t.eq(res.force, true)
+  t.eq(res.x, true)
+})
+
+test('flagless value before flag', t => {
+  setArgv(['hi', '-f'])
+  setConfig({
+    f: { type: 'boolean' },
+  })
+
+  t.eq(res[0], 'hi')
+  t.eq(res.f, true)
+})
+
+test('flagless value after flag', t => {
+  setArgv(['-f', 'hi'])
+  setConfig({
+    f: { type: 'boolean' },
+  })
+
+  t.eq(res[0], 'hi')
+  t.eq(res.f, true)
+})
+
+function runSlurm() {
+  if (config) res = slurm(config)
+}
 
 function setArgv(argv) {
   process.argv = process.argv.slice(0, 2).concat(argv)
+  runSlurm()
+}
+
+function setConfig(newConfig) {
+  config = newConfig
+  runSlurm()
 }
 
 function clearArgv() {
   setArgv([])
-}
-
-function setErrorHandler() {
-  slurm.error = err => {
-    throw err
-  }
 }
